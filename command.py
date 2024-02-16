f0c630ab673cb08ca1661366185a75bfe40c5743
CureSaba <94890302+CureSaba@users.noreply.github.com>
2024-02-16 12:33:07 +0900

    Update command.py

--- command.py
+++ command.py
@@ -4,14 +4,14 @@ import argparse
 parser = argparse.ArgumentParser(description="Generate a license txt file from the excel file containing the key")
 
 parser.add_argument('FileName', help='This is the name of the excel file, by default it is set to a.xlsx', default="a.xlsx")
-parser.add_argument('-i', '--install', default=None, help="Install dependencies using pip, please specify only for the first time")
+parser.add_argument('-i', '--install', action="store_true",help="Install dependencies using pip, please specify only for the first time")
 
 args = parser.parse_args()
-if args.install is not None:
+if args.install:
     import subprocess
     try:
         subprocess.check_call('python3 -m pip install -r requirments.txt', shell=True)
     except:
         print("pip install failed")
-ins=generator(args.FileName)
+ins=generator.genetate(args.FileName)
 ins.main()
